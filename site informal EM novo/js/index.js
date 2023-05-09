81d6b31e0edfe9735793d83038e1f281966466c9
Guidii <101749758+GuilhermeTavares4@users.noreply.github.com>
2023-05-09 19:10:46 -0300

    Update index.js

--- site informal EM novo/js/index.js
+++ site informal EM novo/js/index.js
@@ -1,6 +1,12 @@
-const hamburger = document.querySelector('#hamburger');
+const menuToggle = document.querySelector('.menu-toggle');
+const hamburgerNav = document.querySelector('#hamburger-nav');
+const darken = document.querySelector('.darken');
 
-function hamburgerAction(){
-    let state = getComputedStyle(hamburger, 'left');
-    console.log(state); 
+
+function toggleAction(){
+    //let state = getComputedStyle(hamburger, 'left');
+    //console.log("cu");
+    menuToggle.classList.toggle('isActive');
+    hamburgerNav.classList.toggle('hidden');
+    darken.classList.toggle('show');
 }
